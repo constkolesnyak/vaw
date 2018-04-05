8485a621467b124de54eb2185f6c57d4dbdab1c4
Kostya Kolesnyak <kostya.kolesnyak@yandex.ru>
2018-04-05 23:10:01 +0300

    Add many good features

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-# vk_api_wrap
-This is wrapper for vk_api.
-Go to config.py to use it.
+# VAW (VK API Wrapper)
+
+This is high-level wrapper for [wrapper](https://github.com/python273/vk_api) for [shit](https://vk.com/dev/methods).
+
+## Installation \#TODO
+
+	$ pip install vaw
+
+## Code Example
+
+	from vaw import log_in, user_by_url
+
+	log_in(VK_LOGIN, VK_PASSWORD)
+	mrbirdman = user_by_url('https://vk.com/kostyakolesnyak')
+	mrbirdman.send_message('Hi from vaw!')
+
+## Docs
+
+> Docs is cheap. Show me the [code](https://github.com/mrbirdman2000/vk_api_wrap/blob/master/vaw.py
+)!
+
+• *Linus Torvalds*
+
+## License
+
+This project is licensed under the GNU General Public License – see the LICENSE.md file for detail.
--- vaw.py
+++ vaw.py
@@ -1,10 +1,10 @@
 import vk_api
+from funcy import rpartial, compose, str_join
+from attrdict import AttrDict
 from .config import *
 from collections import namedtuple
 from functools import lru_cache, partial
 from itertools import chain
-from funcy import rpartial, compose, str_join
-from attrdict import AttrDict
 from contextlib import contextmanager
 
 
@@ -73,6 +73,8 @@ raw_get_subscr_users = compose(
 )
 raw_get_followers = temp_func_creator('users.getFollowers', MAX_FOLLOWERS_COUNT_PER_REQUEST)
 raw_get_message_history = temp_func_creator('messages.getHistory', MAX_MESSAGES_COUNT_PER_REQUEST)
+raw_get_group_members = temp_func_creator('groups.getMembers', MAX_GROUP_MEMBERS_COUNT_PER_REQUEST)
+raw_get_likers = temp_func_creator('likes.getList', MAX_LIKERS_COUNT_PER_REQUEST)
 
 del temp_func_creator
 
@@ -88,7 +90,43 @@ class VkObject:
 		return self.id == other.id and type(self) == type(other)
 
 
-class Member(VkObject):
+class Message:
+	def __init__(self, info):
+		self.info = AttrDict(info)
+		self.text = self.info.body
+
+	def __repr__(self):
+		return f'Message ({self.text[:5]}...)'
+
+
+def make_attachment(type, owner_id, item_id):
+	return f'{type}{owner_id}_{item_id}'
+
+
+class Peer(VkObject):
+	def __init__(self, info, peer_id=None):
+		super().__init__(info)
+		self.peer_id = self.id if peer_id is None else peer_id
+
+	def send_message(self, message='', attachments=(), forward_messages=()):
+		return get_api().messages.send(
+			peer_id=self.peer_id,
+			message=message,
+			attachment=','.join(attachments),
+			forward_messages=str_join(',', forward_messages)
+		)
+
+	def get_message_history(self):
+		return map(Message, raw_get_message_history(peer_id=self.peer_id))
+
+	def set_typing(self):
+		get_api().messages.setActivity(
+			peer_id=self.peer_id,
+			type='typing'
+		)
+
+
+class Member(Peer):
 	def __init__(self, info):
 		info = AttrDict(info)
 
@@ -110,6 +148,16 @@ class Member(VkObject):
 	def __repr__(self):
 		return '{} ({})'.format(self.name, self.url)
 
+	def post(self, message='', attachments=(), friends_only=False, from_group=False, signed=False):
+		return get_api().wall.post(
+			owner_id=self.id,
+			message=message,
+			attachments=','.join(attachments),
+			friends_only=int(friends_only),
+			from_group=int(from_group),
+			signed=int(signed)
+		)['post_id']
+
 	def get_posts(self, fields=''):
 		return map(Post, raw_get_posts(
 			owner_id=self.id,
@@ -125,9 +173,6 @@ class Member(VkObject):
 
 	def get_post_by_id(self, post_id, fields=''):
 		return next(self.get_posts_by_ids((post_id,), fields))
-
-	def ban_stories(self):
-		return get_api().stories.banOwner(owners_ids=self.id)
 
 
 def member_id_by_url(url):
@@ -156,6 +201,12 @@ class Group(Member):
 		self.info = AttrDict(info)
 		super().__init__(_to_member_info(info, 'club'))
 
+	def get_members(self, fields=''):
+		return map(User, raw_get_group_members(
+			group_id=-self.id,
+			fields='screen_name,' + fields
+		))
+
 
 def get_group_info(group_id, fields=''):
 	return get_api().groups.getById(
@@ -165,7 +216,10 @@ def get_group_info(group_id, fields=''):
 
 
 group_by_id = compose(Group, get_group_info)
-group_by_url = compose(group_by_id, member_id_by_url)
+
+
+def group_by_url(url, fields=''):
+	return group_by_id(member_id_by_url(url), fields)
 
 
 class User(Member):
@@ -189,7 +243,7 @@ class User(Member):
 	def get_subscr_users(self, fields=''):
 		return map(User, raw_get_subscr_users(
 			user_id=self.id,
-			extended=1,  # important!
+			extended=1,
 			fields='screen_name,' + fields
 		))
 
@@ -215,6 +269,15 @@ class User(Member):
 	def is_app_user(self):
 		return bool(get_api().users.isAppUser(user_id=self.id))
 
+	def is_friend_of(self, user):
+		return user.id in raw_get_friends(user_id=self.id)
+
+	def is_member_of(self, group):
+		return bool(get_api().groups.isMember(
+			user_id=self.id,
+			group_id=-group.id
+		))
+
 
 def get_user_info(user_id, fields=''):
 	return get_api().users.get(
@@ -224,7 +287,10 @@ def get_user_info(user_id, fields=''):
 
 
 user_by_id = compose(User, get_user_info)
-user_by_url = compose(user_by_id, member_id_by_url)
+
+
+def user_by_url(url, fields=''):
+	return user_by_id(member_id_by_url(url), fields)
 
 
 Marked = namedtuple('Marked', 'is_liked is_reposted')
@@ -250,20 +316,54 @@ class Publication(VkObject):
 	def __repr__(self):
 		return '{} ({})'.format(self.type, self.url)
 
-	def marked_by(self, user_id):
-		marked = get_api().likes.isLiked(
-			user_id=user_id,
+	def _useful_publ_info(self):
+		return dict(
 			type=self.type,
 			owner_id=self.owner_id,
 			item_id=self.id
 		)
+
+	def marked_by(self, user):
+		marked = get_api().likes.isLiked(
+			user_id=user.id,
+			**self._useful_publ_info()
+		)
 		return Marked(bool(marked['liked']), bool(marked['copied']))
+
+	def like(self):
+		return get_api().likes.add(**self._useful_publ_info())
+
+	def unlike(self):
+		return get_api().likes.delete(**self._useful_publ_info())
+
+	def get_likers_ids(self, friends_only=False):
+		return raw_get_likers(
+			**self._useful_publ_info(),
+			friends_only=int(friends_only)
+		)
+
+	def get_reposters_ids(self, friends_only=False):
+		return raw_get_likers(
+			**self._useful_publ_info(),
+			friends_only=int(friends_only),
+			filter='copies'
+		)
+
+	def repost(self, message='', group=None):
+		return get_api().wall.repost(
+			object=make_attachment(**self._useful_publ_info()).replace('post', 'wall'),
+			message=message,
+			group_id='' if group is None else -group.id
+		)['post_id']
 
 
 def _to_publication_info(info, commented_member_id=None, commented_publication_id=None):
 	info = AttrDict(info)
 
-	owner_id = info.get('owner_id', commented_member_id)
+	try:
+		owner_id = info['to_id']
+	except KeyError:
+		owner_id = info.get('owner_id', commented_member_id)
 	if commented_publication_id is None:
 		url_end = info.id
 	else:
@@ -286,19 +386,42 @@ class Post(Publication):
 		super().__init__(_to_publication_info(info))
 		self._to_comment = rpartial(Comment, self.owner_id, self.id)
 
+	def _useful_post_info(self):
+		return dict(
+			owner_id=self.owner_id,
+			post_id=self.id
+		)
+
+	def delete(self):
+		return get_api().wall.delete(**self._useful_post_info())
+
 	def get_comments(self, fields=''):
 		return map(self._to_comment, raw_get_comments(
-			owner_id=self.owner_id,
-			post_id=self.id,
+			**self._useful_post_info(),
 			preview_length=0,
 			fields=fields
 		))
 
-	def delete(self):
-		return get_api().wall.delete(
-			owner_id=self.owner_id,
-			post_id=self.id
+	def comment(self, message='', attachments=()):
+		return get_api().wall.createComment(
+			**self._useful_post_info(),
+			message=message,
+			attachments=','.join(attachments)
 		)
+
+	def pin(self):
+		return get_api().wall.pin(**self._useful_post_info())
+
+	def unpin(self):
+		return get_api().wall.unpin(**self._useful_post_info())
+
+
+def post_by_url(url, fields=''):
+	owner, post = url.split('wall')[-1].split('_')
+	return Post(get_api().wall.getById(
+		posts=owner + '_' + post,
+		fields=fields
+	)[0])
 
 
 class Comment(Publication):
@@ -313,21 +436,45 @@ class Comment(Publication):
 	def __eq__(self, other):
 		return super().__eq__(other) and self.commented_publication_id == other.commented_publication_id
 
+	def reply(self, message='', attachments=()):
+		return get_api().wall.createComment(
+			owner_id=self.commented_member_id,
+			post_id=self.commented_publication_id,
+			message=message,
+			reply_to_comment=self.id,
+			attachments=','.join(attachments)
+		)
 
-class Chat(VkObject):
+	def delete(self):
+		return get_api().wall.deleteComment(
+			owner_id=self.commented_member_id,
+			comment_id=self.id
+		)
+
+
+class Chat(Peer):
 	def __init__(self, info):
 		self.info = AttrDict(info)
 
-		super().__init__(dict(
-			id=2000000000 + self.info.id,
-			url=BASE_VK_URL + 'im?sel=c' + str(self.info.id)
-		))
+		super().__init__(
+			dict(
+				id=self.info.id,
+				url=BASE_VK_URL + 'im?sel=c' + str(self.info.id)
+			),
+			2000000000 + self.info.id
+		)
 
 	def retitle(self, new_title):
 		return get_api().messages.editChat(
 			chat_id=self.id,
 			title=new_title
 		)
+
+	def get_users(self, fields=''):
+		return map(User, get_api().messages.getChatUsers(
+			chat_id=self.id,
+			fields='screen_name,' + fields
+		))
 
 
 def chat_by_id(chat_id, fields=''):
@@ -337,33 +484,9 @@ def chat_by_id(chat_id, fields=''):
 	))
 
 
-chat_by_url = compose(chat_by_id, lambda url: url.split('c')[-1])
+def chat_id_by_url(url):
+	return int(url.split('c')[-1])
 
 
-def to_attachment(att_type, owner_id, obj_id):
-	return '{}{}_{}'.format(att_type.value, owner_id, obj_id)
-
-
-def send_message(peer, message='', attachments=(), forward_messages=()):
-	return get_api().messages.send(
-		peer_id=peer.id,
-		message=message,
-		attachment=','.join(attachments),
-		forward_messages=str_join(',', forward_messages)
-	)
-
-
-class Message:
-	def __init__(self, info):
-		self.info = AttrDict(info)
-		self.text = self.info.body
-
-	def __repr__(self):
-		return f'Message({self.text[:5]}...)'
-
-
-def get_message_history(peer):
-	return map(Message, raw_get_message_history(peer_id=peer.id))
-
-
-get_last_message = compose(next, get_message_history)
+def chat_by_url(url, fields=''):
+	return chat_by_id(chat_id_by_url(url), fields)
